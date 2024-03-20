8d8c28cb358148d0bffa5da93918c5428e278c2c
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2024-03-21 00:26:15 +0800

    Update entrypoint.sh

--- actions/veux/entrypoint.sh
+++ actions/veux/entrypoint.sh
@@ -12,7 +12,7 @@ msg "Updating container..."
 pacman -Syu --noconfirm > /dev/null 2>&1
 
 msg "Installing prerequisites..."
-pacman -S --noconfirm wget make zip tar binutils gcc > /dev/null 2>&1
+pacman -S --noconfirm wget make zip tar binutils gcc flex > /dev/null 2>&1
 
 cd source
 workdir=$(pwd)
