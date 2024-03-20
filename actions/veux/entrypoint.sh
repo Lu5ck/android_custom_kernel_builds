f83bcc9269915880137e5d9e01d4a5958a17a5c0
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2024-03-21 01:05:55 +0800

    Update entrypoint.sh

--- actions/veux/entrypoint.sh
+++ actions/veux/entrypoint.sh
@@ -12,7 +12,7 @@ msg "Updating container..."
 pacman -Syu --noconfirm > /dev/null 2>&1
 
 msg "Installing prerequisites..."
-pacman -S --noconfirm curl wget git make zip tar binutils gcc flex bison bc > /dev/null 2>&1
+pacman -S --noconfirm curl wget git make zip tar binutils gcc flex bison bc hostname cmp > /dev/null 2>&1
 
 cd source
 workdir=$(pwd)
