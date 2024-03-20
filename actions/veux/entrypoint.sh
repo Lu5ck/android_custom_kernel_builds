016c5605bce1bdb419163c06c830c729be560ad0
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2024-03-20 23:59:12 +0800

    Update entrypoint.sh

--- actions/veux/entrypoint.sh
+++ actions/veux/entrypoint.sh
@@ -5,15 +5,27 @@ msg(){
     echo "===================================="
 }
 
-workdir="$GITHUB_WORKSPACE"
-
 free
 df -h
 
 msg "Updating container..."
 pacman -Syu --noconfirm
 
+cd source
+workdir=pwd
+
+msg "Removing imported KSU, if any"
+if [ -d "drivers/kernelsu" ]; then
+    rm -rf "drivers/kernelsu"
+fi
+
+msg "Get latest KSU"
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+
 echo $workdir
+cd $workdir
 
-ls
+msg "Grant executable rights to builtin script"
+chmod +x build_kernel.sh
 
+build_kernel.sh
