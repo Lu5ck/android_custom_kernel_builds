c136adf42baf51f307f5d354a6a806ef029e4509
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2024-03-21 00:11:26 +0800

    Update entrypoint.sh

--- actions/veux/entrypoint.sh
+++ actions/veux/entrypoint.sh
@@ -12,7 +12,6 @@ msg "Updating container..."
 pacman -Syu --noconfirm > /dev/null 2>&1
 
 cd source
-pwd
 workdir=$(pwd)
 
 if [ -d "drivers/kernelsu" ]; then
@@ -23,12 +22,10 @@ fi
 msg "Get latest KSU"
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
 
-echo $workdir
 cd $workdir
 
 msg "Grant executable right to builtin script"
 chmod +x build_kernel.sh
 
 msg "running Builtin script"
-ls
-build_kernel.sh
+./build_kernel.sh
