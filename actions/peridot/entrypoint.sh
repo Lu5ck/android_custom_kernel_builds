a98179a47a5438a32160f458c5b523f4597488bc
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-02-27 14:13:29 +0800

    Update entrypoint.sh

--- actions/peridot/entrypoint.sh
+++ actions/peridot/entrypoint.sh
@@ -41,21 +41,21 @@ mkdir toolchain && (cd toolchain; bash <(curl -s "https://raw.githubusercontent.
 
 
 export PATH=$(pwd)/toolchain/bin/:$PATH
-export BUILD_CC="$(pwd)/toolchains/neutron-clang/bin/clang"
+export BUILD_CC="$(pwd)/toolchain/neutron-clang/bin/clang"
 export ARCH=arm64
 export SUBARCH=arm64
 export DISABLE_WRAPPER=1
 KERNEL_DEFCONFIG="gki_defconfig vendor/peridot_GKI.config vendor/custom.config"
 KERNEL_CMDLINE="ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=out LLVM=1 LLVM_IAS=1 \
-AR=$(pwd)/toolchains/neutron-clang/bin/llvm-ar \
-NM=$(pwd)/toolchains/neutron-clang/bin/llvm-nm \
-LD=$(pwd)/toolchains/neutron-clang/bin/ld.lld \
-STRIP=$(pwd)/toolchains/neutron-clang/bin/llvm-strip \
-OBJCOPY=$(pwd)/toolchains/neutron-clang/bin/llvm-objcopy \
-OBJDUMP=$(pwd)/toolchains/neutron-clang/bin/llvm-objdump \
-READELF=$(pwd)/toolchains/neutron-clang/bin/llvm-readelf \
-HOSTCC=$(pwd)/toolchains/neutron-clang/bin/clang \
-HOSTCXX=$(pwd)/toolchains/neutron-clang/bin/clang++"
+AR=$(pwd)/toolchain/neutron-clang/bin/llvm-ar \
+NM=$(pwd)/toolchain/neutron-clang/bin/llvm-nm \
+LD=$(pwd)/toolchain/neutron-clang/bin/ld.lld \
+STRIP=$(pwd)/toolchain/neutron-clang/bin/llvm-strip \
+OBJCOPY=$(pwd)/toolchain/neutron-clang/bin/llvm-objcopy \
+OBJDUMP=$(pwd)/toolchain/neutron-clang/bin/llvm-objdump \
+READELF=$(pwd)/toolchain/neutron-clang/bin/llvm-readelf \
+HOSTCC=$(pwd)/toolchain/neutron-clang/bin/clang \
+HOSTCXX=$(pwd)/toolchain/neutron-clang/bin/clang++"
 make $KERNEL_CMDLINE $KERNEL_DEFCONFIG 
 make $KERNEL_CMDLINE -j$(nproc --all)
 
