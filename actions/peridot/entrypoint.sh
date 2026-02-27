50579f3fa29fcb244f7078a88e9fac0bbe4c3a6a
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-02-27 12:44:18 +0800

    Create entrypoint.sh

---
+++ actions/peridot/entrypoint.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+msg(){
+    echo
+    echo "==> $*"
+    echo
+}
+
+free
+df -h
+
+msg "Updating container..."
+pacman -Syu --noconfirm > /dev/null 2>&1
+
+msg "Installing prerequisites..."
+pacman -S --noconfirm curl wget git make zip tar binutils gcc flex bison bc inetutils diffutils > /dev/null 2>&1
+
+cd source
+workdir=$(pwd)
+config_file="$workdir/arch/arm64/configs/gki_defconfig"
+
+if [ -d "drivers/kernelsu" ]; then
+    msg "Removing imported KSU"
+    rm -rf "drivers/kernelsu"
+    sed -i '/^source "drivers\/kernelsu\/Kconfig"$/d' drivers/Kconfig
+fi
+
+msg "Get latest KSU"
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/refs/heads/next-susfs-a14-6.1-dev/kernel/setup.sh"
+
+msg "Enable KSU flag"
+if grep -q "^CONFIG_KSU=n$" "$config_file"; then
+    sed -i 's/^CONFIG_KSU=n$/CONFIG_KSU=y/' "$config_file"
+elif grep -q "^CONFIG_KSU=y$" "$config_file"; then
+    :
+else
+    echo "CONFIG_KSU=y" >> "$config_file"
+fi
+
+msg "Downloading toolchain"
+mkdir toolchain && (cd toolchain; bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S)
+
+KERNEL_DEFCONFIG="gki_defconfig vendor/pineapple_GKI.config vendor/peridot_GKI.config vendor/custom.config"
+KERNEL_CMDLINE="ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=out LLVM=1"
+export PATH=$(pwd)/toolchain/bin/:$PATH
+export ARCH=arm64
+export SUBARCH=arm64
+export DISABLE_WRAPPER=1
+make $KERNEL_CMDLINE $KERNEL_DEFCONFIG 
+make $KERNEL_CMDLINE -j$(nproc --all)
+
+msg "Preparing AnyKernel3"
+cd $workdir
+ls out/arch/arm64/boot/
+cp out/arch/arm64/boot/Image "../builder/actions/peridot/AnyKernel3" || exit 1
