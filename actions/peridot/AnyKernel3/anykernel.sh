84199d1dcfbcbd95e2bfd893f32fbb3d3c82b6d1
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-09-25 16:07:34 +0800

    Update anykernel.sh

--- actions/peridot/AnyKernel3/anykernel.sh
+++ actions/peridot/AnyKernel3/anykernel.sh
@@ -32,7 +32,7 @@ ui_print " "
 
 kernel_version=$(strings Image | grep -E -m1 'Linux version.*#' | cut -d\\  -f3-)
 
-ui_print "Flashing KSUN+SUSFS LOS Kernel..."
+ui_print "Flashing KSU+SUSFS LOS Kernel..."
 
 # boot install
 if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
@@ -49,6 +49,6 @@ ui_print "Kernel version: $kernel_version"
 
 ui_print " "
 
-ui_print "Flashed KSUN+SUSFS LOS Kernel successfully!"
+ui_print "Flashed KSU+SUSFS LOS Kernel successfully!"
 
 ## end boot install
